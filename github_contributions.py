a42ed05fc9857e1936a8bdf7334db3fddc251cc0
BEPb <andrej.marinchenko@gmail.com>
2021-11-06 18:04:52 +0300

    upd

--- github_contributions.py
+++ github_contributions.py
@@ -15,65 +15,70 @@ from datetime import date, timedelta  # подключаем модуль для
 
 
 def commit(days_ago, msg):  # функция коментария (указываем на сколько дней назад делаем сдвиг, сообщение)
-  d = date.today() - timedelta(days = days_ago)  # высчитываем день начала рисунка (первого комментария)
+  # d = date.today() - timedelta(days = days_ago)  # высчитываем день начала рисунка (первого комментария)
+  d = off_day - timedelta(days = days_ago)  # высчитываем день начала рисунка (первого комментария)
   t = str(d) + " 00:00:00"  # строчное значение полученной величины + " 00:00:00"
   os.system("echo " + msg + " > .tmpfile")
   os.system("git add .tmpfile")
   os.system('GIT_COMMITTER_DATE="'+t+'"' + ' GIT_AUTHOR_DATE="'+t+'"' + ' git commit -m "' + msg + '" 2>&1 >/dev/null')
+  print('GIT_COMMITTER_DATE="'+t+'"' + ' GIT_AUTHOR_DATE="'+t+'"' + ' git commit -m "' + msg + '" 2>&1 >/dev/null')
 
 def rgb2gray(rgb):  # преобразовываем цветной рисунок в серый
-    r, g, b = rgb[0:3] # выделяем три переменные r,g,b, параметр alpha не учитываем
+    r, g, b = rgb[0:3]  # выделяем три переменные r,g,b, параметр alpha не учитываем
     gray = 0.2989 * r + 0.5870 * g + 0.1140 * b  # преобразуем в оттенок серого
     return gray  # функция возвращает результат, оттенок серого
 
-def write_px(x, y, intensity, prefix=""):  # рисуем пиксели (координаты, интенсивность)
-  days_ago = numdays+offset - (x*rows+y)  # дней назад = количество дней на странице + учитываем какой сегодня день
+def write_px(x, y, intensity, prefix=""):  # записываем коммиты на git (координаты, интенсивность)
+  days_ago = numdays + offset - (x*rows+y)  # дней назад = количество дней на странице + учитываем какой сегодня день
   # недели - смещение по оси х умноженное на число строк + у
-  d = date.today() - timedelta(days = days_ago)  # от сегодняшнего дня отнимаем количество дельта дней
-  t = str(d) + " 00:00:00"  # преобразуем в строчную величину и добавляем " 00:00:00"
+  # d = date.today() - timedelta(days = days_ago)  # от сегодняшнего дня отнимаем количество дельта дней
+  d = off_day - timedelta(days = days_ago)  # от сегодняшнего дня отнимаем количество дельта дней
   print("val=", intensity, "x", x, "y", y, "date:", d)  # выводим полученные данные
+  intensity = int(intensity)
   for i in range(0, intensity):  # в зависимости от параметра интенсивности в этот день (определяется количество
     # коммитов)
-    msg = prefix + os.urandom(8).encode("hex")  # сообщение
+    priv = os.urandom(8)
+    msg = prefix + priv.hex()  # сообщение
     commit(days_ago, msg)
 
 
 def process_image(path):  # функция работы с рисунком 52x7
-  img = Image.open(path)
-  px = img.load()
-  size = img.size
-  if (52,7) != size:
-    raise Exception("Image should be 52x7, got " + size)
-  for x in range(size[0]):
-    print("processed line", x)
-    for y in range(size[1]):
-      val = 255-int(rgb2gray(px[x, y]))
-      val /= 16
-      write_px(x, y, val, prefix="ign-")
-
-def process_text(txt, offset=2):  # функция преобразования текста (фразы) в рисунок
-  f = 1
-  image = Image.new("RGB", [x*f for x in size], (255, 255, 255))
-  draw = ImageDraw.Draw(image)
-# see https://mail.python.org/pipermail/image-sig/2005-August/003497.html
-  draw.fontmode = "1"
-  font = ImageFont.truetype("font/5x5_pixel.ttf", 8)
-  draw.text((offset, 1), txt, (0, 0, 0), font=font)
-  image.save(txt+".bmp")  # сохраняем изображение в файл .bmp
+  img = Image.open(path)  # открываем рисунок
+  px = img.load()  # создаем таблицу данных рисунка
+  size = img.size  # получаем размер рисунка
+  if (52, 7) != size:  # Если рисунок не 52x7
+    raise Exception("Рисунок должен быть 52x7, Ваш рисунок имеет размер: " + size)
+  for x in range(size[0]):  # перемещаемся по рядам от 1 до 7
+    print("Ведется работа над рядом №: ", x)  # печатаем № ряда
+    for y in range(size[1]):  # перемещаемся по столбцам от 1 до 52
+      val = 255 - int(rgb2gray(px[x, y]))  # преобразуем цвет в оттенок серого
+      val /= 16  # оттенок серого преобразуем в значения от 1 до 4
+      write_px(x, y, val, prefix="ign-")  # обращаемся к функции записи коммита
 
 def main():
-  if sys.argv[1] == "--text":  # если программа запущена с аргументом "--text", то коммит делается из фразы
-    process_text(sys.argv[2])
-  else:  # иначе коммит делается из рисунка
-    process_image(sys.argv[1])
+    process_image('test.png')  # коммит делается из рисунка
+    # print('now  ', now)
+    # print('offset  ', offset)
+    # print('d ', d)
+    # print('date.today() ', date.today())
+    # print('date.today().weekday()', date.today().weekday())
 
 # Определяем используемые переменные
 # GitHub начинает неделю с воскресенья, добавим 1 (какой сегодня день недели???)
-offset = (date.today().weekday() + 300) % 7  # от сегодняшней даты, формируем матрицу 7 на 52
+# offset = (date.today().weekday() + 1) % 7  # сегодняшней даты, на матрице 7 на 52 (от этого зависит какой день
+now = date.today()
+off_day = date(2020, 12, 20)
+days_ago = now - off_day
+# print('days_ago ', days_ago)
+offset = (date.today().weekday() + 1) % 7  # сегодняшней даты, на матрице 7 на 52 (от этого зависит какой день
+# d = date.today() - timedelta(days = days_ago.days)
+# выбрать начальным для рисования рисунка)
+# ear_ago = 3  # определяем сколько лент назад рисовать коммит рисунок
+# first_day = str('2020-01-04')  # первый день
 rows = 7  # строки
 cols = 52  # столбцы
 size = (cols, rows)  # общий размер 7 на 52
-numdays = rows * cols  # количество дней на одной странице
+numdays = rows * cols  # количество дней на одной странице 7 x 52 = 354
 
 if __name__ == "__main__":
   main()
